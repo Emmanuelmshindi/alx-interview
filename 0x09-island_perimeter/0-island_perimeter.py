4739dc52051f791294148b37e5fe5f3d85b761e0
Emmanuelmshindi <mshindimanuel@gmail.com>
2024-02-07 18:53:47 +0000

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,7 +22,7 @@ def island_perimeter(grid):
         perim += dfs(i - 1, j)
         return perim
 
-    for i in range (len(grid)):
-        for j in range (len(grid[0])):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j]:
                 return dfs(i, j)
